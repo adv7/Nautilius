f44c1b20da826076a2162f1576ce184469515b4b
Krystian <cud4xx@gmail.com>
2023-09-05 20:19:31 +0200

    initial commit

---
+++ Source/Nautilius/NautiliusGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NautiliusGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NAUTILIUS_API ANautiliusGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
